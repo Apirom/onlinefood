711dbc3ede7e8a35d6143afcb841df6c43a981fc
molysulfur <molysulfur.mos@gmail.com>
2018-08-21 16:01:16 +0700

    count down splash screen

--- app/src/main/java/com/molysulfur/onlinefood/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/molysulfur/onlinefood/ui/splash/SplashActivity.kt
@@ -1,10 +1,13 @@
 package com.molysulfur.onlinefood.ui.splash
 
+import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.os.CountDownTimer
 import com.molysulfur.onlinefood.R
 import com.molysulfur.onlinefood.databinding.ActivitySplashBinding
 import com.molysulfur.onlinefood.ui.base.BaseActivity
+import com.molysulfur.onlinefood.ui.main.MainActivity
 import javax.inject.Inject
 
 class SplashActivity : BaseActivity(){
@@ -19,5 +22,21 @@ class SplashActivity : BaseActivity(){
         mViewDataBinding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
         mViewDataBinding.viewModel = mSplashViewModel
         mViewDataBinding.executePendingBindings()
+        initInstaces()
+    }
+
+    private fun initInstaces() {
+        object : CountDownTimer(3000,1000){
+            override fun onFinish() {
+                val intent = Intent(baseContext,MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+            }
+
+        }.start()
+
     }
 }
